533f159f3350db68fbca8635d3dfb4848a3f81dd
Usman12345678910 <Usman12345678910>
2019-04-04 23:25:39 -0700

    update Node indax page

--- Firstpage.js
+++ Firstpage.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function(req,res){
 res.writeHead(200,{'Content-Type':'text/html'});
-res.end('<h1>Hello Class:This is First Server Page</h1>');
+res.end('<h1>Hello Class:HI Usman Madni Kasa ha</h1>');
 
 }).listen(8080);
