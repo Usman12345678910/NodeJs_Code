38722e788c9b27f9f0efc0da53443e60a366e0e1
Usman12345678910 <Usman12345678910>
2019-04-04 22:43:02 -0700

    NodeJs 5

---
+++ Firstpage.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function(req,res){
+res.writeHead(200,{'Content-Type':'text/html'});
+res.end('<h1>Hello Class:This is First Server Page</h1>');
+
+}).listen(8080);
